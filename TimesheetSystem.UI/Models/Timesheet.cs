d3d29140d42d6e1e01765d72a403a9c4db95aa1c
rscomms <rs.comms2@gmail.com>
2024-11-24 14:46:12 +0000

    Model updated

--- TimesheetSystem.UI/Models/Timesheet.cs
+++ TimesheetSystem.UI/Models/Timesheet.cs
@@ -2,13 +2,15 @@
 {
     public class Timesheet
     {
-        public int EntryID { get; set; }
-        public string EmployeeName { get; set; }
-        public DateOnly EntryDate { get; set; }
-
+        public int ID { get; set; }
+        //TO DO: replace this with UserID later (Not required for this demo)
+        public string UserName { get; set; }
+        public DateOnly Date { get; set; }
+        //TO DO: replace this with ProjectID
         public string ProjectName { get; set; }
-        public string Description { get; set; }
+        public string TaskDesc { get; set; }
 
         public int HoursWorked { get; set; }
+        public int TotalHoursWorked { get; set; }
     }
 }
